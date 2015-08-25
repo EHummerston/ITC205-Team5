18c6d32f0b3e7ab96a79b986905fa149d8317b61
Jackson Cribb <cribbjackson@gmail.com>
2015-08-25 20:19:03 +1000

    Fixed #39 - ListStudentsCtl restored

---
+++ src/datamanagement/ListStudentsCtl.java
@@ -0,0 +1,16 @@
+package datamanagement;
+
+public class ListStudentsCtl {
+  private StudentManager studentManager_;
+
+  public ListStudentsCtl() {
+    studentManager_ = StudentManager.get();
+  }
+
+  public void listStudents(IStudentLister studentLister, String unitCode) {
+    studentLister.clearStudents();
+    StudentMap students = studentManager_.getStudentsByUnit(unitCode);
+    for (Integer id : students.keySet()) studentLister.
+            addStudent(students.get(id));
+  }
+}
